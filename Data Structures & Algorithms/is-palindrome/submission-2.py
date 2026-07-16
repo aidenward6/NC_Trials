20bef5d91556fa5aac4de74cbe7e334224526b26
Aiden Ward <126358242+aidenward6@users.noreply.github.com>
2026-07-15 21:14:59 -0400

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        s=s.translate(str.maketrans("", "", ",.:'?"))
+        nospace_s = s.replace(" ", "")
+        reverse_string = nospace_s[::-1]
+        if nospace_s == reverse_string:
+            return True
+        else:
+            return False
+
+        
